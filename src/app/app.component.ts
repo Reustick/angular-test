ae2baf3ca1d75fea92c157909c8f0ce8f874feb6
Rustam <rustam391@gmail.com>
2020-08-06 11:19:39 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,32 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  valString:string // строка получаемая из формы ввода
-  valToJSON; // сюда будет записан массив объектов из строки valString, из него отрисовывается наполнение карточек
-  titles = []; // здесь будет массив ключей объектов в valToJSON, из него отрисовываются заголовки карточек
-  isReadyToReturn = false; // флаг, отражает состояние приложения (ожидает строку от пользователя или готово это строку (возможно видоизмененную) вернуть)
-  
-  // метод преобразует введенную пользователем строку в карточки с наполнением
+  valString:string 
+  valToJSON; 
+  titles = []; 
+  isReadyToReturn = false; 
   takeMyString() {
-    this.valToJSON = JSON.parse(this.valString); // парсим пользовательскую строку, в этот момент отрисовываются карточки
-    this.valToJSON.forEach(element => { // заполняем titles, отрисовываюся заголовки карточек
+    this.valToJSON = JSON.parse(this.valString); 
+    this.valToJSON.forEach(element => { 
       Object.keys(element).filter((item) => {
         let name = item.slice(0,1).toUpperCase() + item.slice(1)
         this.titles.includes(name)? '': this.titles.push(name);
       })
     })
-    this.isReadyToReturn = !this.isReadyToReturn; // переключаем флаг
-  }
-
-  // метод возвращает значения из полей обратно в виде строки сохранив все изменения
+    this.isReadyToReturn = !this.isReadyToReturn; 
+  } 
   giveMyStringBack() {
-    // когда пользователь меняет значения в полях - он меняет их в массиве объектов valToJSON,
-    // поэтому мы приводим объект к виду строки и присваиваем это значение valString
     this.valString = JSON.stringify(this.valToJSON); 
-    this.isReadyToReturn = !this.isReadyToReturn; // переключаем флаг
+    this.isReadyToReturn = !this.isReadyToReturn; 
   }
-
-  // метод определяет в каком состоянии флаг и решает забирать строку или отдавать
   change() {
     this.isReadyToReturn ? this.giveMyStringBack() : this.takeMyString();
   }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,54 +6,43 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-
 export class FormComponent {
-
-  @Input() val:string; // получаем переменную valString, она будет связана с полем ввода
+  @Input() val:string; 
   @Output() valChange = new EventEmitter<any>() 
-  @Input() isReadyToReturn:boolean; // получаем состояние флага для синхронизации
-
+  @Input() isReadyToReturn:boolean; 
 
   myForm: FormGroup;
   
   constructor(private fb: FormBuilder) {
     this._createForm()
   }
-  // инициируем форму
   private _createForm() {
     this.myForm = this.fb.group({
       val: [
         '',
-        [// обозначаем валидацию (поле обязательно, и должно быть массивом JSON объектов)
+        [
           Validators.required,
           Validators.pattern(/^\[({(,?\s?"[\wа-яА-ЯёЁ]+"\s?:\s?"[^".?]*")+},?\s?)+\]$/)
         ]
       ]
     })
   }
-
-  btnText:string = 'продолжить' // текст кнопки по умолчанию
-
-  // когда форма сабмититься
+  btnText:string = 'продолжить' 
   onSubmit() {
-    if (this.isReadyToReturn) {// если сабмит происходит когда приложение готово вернуть строку (состояние, когда мы можем работать с карточками)
-      this.btnText = 'продолжить';// меняется текст кнопки
-      this.valChange.emit();// эмитируется событие (нужно для срабатывания внешних методов)
+    if (this.isReadyToReturn) {
+      this.btnText = 'продолжить';
+      this.valChange.emit();
       setTimeout(() => {
-        // таймаут нужен чтобы метод сработал в нужный момент, 
-        // без него срабатывает поздно. Подозреваю что это связано с колстеком, такой вот небольшой хак.
-        // (вообще я не люблю хаки, но тут не понял как иначе сделать)
-        this.myForm.patchValue({val: this.val}) // изменяем содержимое поля ввода формы, в соответствии с обновленными полями ввода карточек
+        this.myForm.patchValue({val: this.val}) 
       }, 0)
-      this._val.enable(); // делаем поле ввода доступным
-    } else { // если сабмит происходит когда приложение готово принять строку (состояние в котором доступно поле ввода и не доступно редактирование карточек)
-      this.btnText = 'выгрузить' // меняется текст кнопки
-      this.valChange.emit(this._val.value); // передается наружу значение поля ввода, и одновременно эмитируется событие (нужно для срабатывания внешних методов)
-      this._val.disable();  // поле ввода становится недоступным
+      this._val.enable(); 
+    } else { 
+      this.btnText = 'выгрузить' 
+      this.valChange.emit(this._val.value); 
+      this._val.disable();  
     }
   }
-
-  get _val() {//получаем сокращенную запись доступа к полю ввода (используется много раз)
+  get _val() {
     return this.myForm.get('val')
   }
 }
